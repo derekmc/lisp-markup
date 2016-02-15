45273c18021c0abd35fb39cc4cd907af7e245f71
user <derek7mc@gmail.com>
2016-02-14 20:35:53 -0700

    restructured module and some documentation.

--- list2markup.js
+++ list2markup.js
@@ -1,17 +1,62 @@
 
+/*
+ * List2Markup: turn a lisp-style list datastructure into markup such as html. 
+ * 
+ * Lisp2Markup has built in conversion functions and you may create your own conversion
+ * functions by providing a taghandler.
+ *
+ * Built-In Conversions:
+ *    toHtml: convert to html.
+ *
+ * To make a custom markup conversion function, use the following function.
+ *
+ * customMarkupConverter(taghandler):
+ *   creates a custom markup conversion function using the provided taghandler function.
+ *
+ * All markup conversion functions follow the following calling pattern.
+ *
+ * markupConverter( l,data):
+ *   l: the lisp-like list datastructure to be converted to markup.
+ *     entries in l are handled accoring to the type of the entry:
+ *       - lists within this list are evaluated recursively like in lisp.
+ *       - objects are property sets which are added to current node in the markup.
+ *       - a function in the first position of a list is a macro.
+ *       - a function in a non-first position is a template function.
+ *       - templates in l are called with just the data paramter.
+ *          template( data)
+ *          templates must return a string.
+ *       - macros in l are called with the following parameters:
+ *          macro( l,data,markupConverter)
+ *            - l: the current list this macro is applied to,
+ *                 which will include the macro itself in the first position.
+ *            - data: for templating, this is the same parameter that is passed to markupCoverter( l,data), see more below.
+ *            - markupConverter: macros are provided the current markupConverter function so they can do more magic.
+ *
+ *            The macro's return value is used in place of the original list with the macro.
+ *            If it returns a string, that string is inserted into the markup.
+ *            If it returns a list, the list is evaluated by the markupConverter
+ *            If it returns an object, the properties are added to the parent node in the markup.
+ *            It may not return a function.
+ *   data:
+ *     this parameter allows your list datastructure to be used as a template.
+ *     the values in the final markup are filled in with the data values from this list.
+ *     Both template and macro functions within your datastructure are passed this parameter as described above.
+ */ 
+
 
 (function(exports){
   // TODO use real typecheck module
   function T(){}
 
-  /* defaultTagHandler: process html tags
+  /* htmlTagHandler: process html tags
    * parses tagname and optionally id and classes
    * id starts with a hash "#"
    * classes start with a period "."
    *
    * returns [opentag, closetag]
    */
-  exports.defaultTagHandler = function(tagstr, props){
+  exports.htmlTagHandler = htmlTagHandler;
+  function htmlTagHandler(tagstr, props){
       T(tagstr, "");
       T(props, {}, undefined);
       if(!props) props = {}
@@ -59,76 +104,67 @@
           close = "</" + tagname + ">";
       return [open, close];
   }
-  /*
-   *   taghandlers: [[name, match, handler]]
-   *     'match' is used to determine if a given handler should process a set of tags
-   *     iff match(tagstr) returns true, then handler(tagstr, props) is called
-   *     'handler' should return [opentag, closetag]
-   */
+  
+  exports.customMarkupConverter = customMarkupConverter;
+  exports.toHtml = customMarkupConverter(htmlTagHandler);
 
-  /* list2markup: turn a lisp-style list datastructure into html 
-   * parameters:
-   *   l:
-   *     the list datastructure to be converted.
-   *     - an object is a set of properties
-   *     - a function in the first position of a list is a macro.
-   *     - a function in a non-first position is a template function.
-   *   data:
-   *     data parameter to macro or template functions.
-   */ 
-  exports.list2markup = function(l, data){
-      T(l, []);
-      T(data, {}, [], undefined);
-      var props = {},
-          first = l[0],
-          parts = [''];  //save space for opening tag
-      if(typeof first == "function"){
-          // first is a macro
-          var macro = first;
-          var macro_result = macro(l, data, exports.list2markup);
-          if(typeof macro_result == "string"){
-              return macro_result; }
-          else if(Array.isArray(macro_result)){
-              return exports.list2markup(macro_result, data); }
-          else if(!macro_result){
-              return ""; }
-          else{
-              throw "list2markup: macro returned value with invalid type."; }
-          // returned
-      }
-
-      var tagname = null;
-      if(typeof first == "string"){
-          tagname = first; }
-
-      for(var i=0; i<l.length; ++i){
-          if(i==0 && tagname) continue; //skip tag
-          var x = l[i];
-          if(Array.isArray(x)){
-              parts.push(exports.list2markup(x, data)); }
-          else if(typeof x == "object"){
-              for(var k in x){
-                  props[k] = x[k]; }}
-          else if(typeof x == "function"){
-              // template function
-              var template = x;
-              var template_result = template(data);
-              if(typeof template_result == "string"){
-                  parts.push(template_result); }
-              else if(!template_result){
-                  parts.push(""); }
+  function customMarkupConverter(taghandler){
+      if(typeof taghandler != "function"){
+          throw "List2Markup.customMarkupConverter: taghandler argument must be a function"; }
+      
+      return markupConverter;
+      function markupConverter(l, data){
+          T(l, []);
+          T(data, {}, [], undefined);
+          var props = {},
+              first = l[0],
+              result_parts = [''];  //save space for opening tag
+          if(typeof first == "function"){
+              // first is a macro
+              var macro = first;
+              var macro_result = macro(l, data, markupConverter);
+              if(typeof macro_result == "string"){
+                  return macro_result; }
+              else if(Array.isArray(macro_result)){
+                  return markupConverter(macro_result, data); }
+              else if(!macro_result){
+                  return ""; }
               else{
-                  throw "list2markup: template returned value with invalid type."; }}
-          else{
-               if(x) parts.push(x.toString()); }
+                  throw "List2Markup markup conversion function: macro returned value with invalid type."; }
+              // returned
+          }
+
+          var tagname = null;
+          if(typeof first == "string"){
+              tagname = first; }
+
+          for(var i=0; i<l.length; ++i){
+              if(i==0 && tagname) continue; //skip tag
+              var x = l[i];
+              if(Array.isArray(x)){
+                  result_parts.push(markupConverter(x, data)); }
+              else if(typeof x == "object"){
+                  for(var k in x){
+                      props[k] = x[k]; }}
+              else if(typeof x == "function"){
+                  // template function
+                  var template = x;
+                  var template_result = template(data);
+                  if(typeof template_result == "string"){
+                      result_parts.push(template_result); }
+                  else if(!template_result){
+                      result_parts.push(""); }
+                  else{
+                      throw "List2Markup markup conversion function: template returned value with invalid type."; }}
+              else{
+                   if(x) result_parts.push(x.toString()); }
+          }
+          if(tagname){
+              var tags = handler(tagname, props);
+              result_parts[0] = tags[0];
+              result_parts.push(tags[1]); }
+          return result_parts.join('');
       }
-      if(tagname){
-          // TODO custom tag handlers
-          var handler = exports.defaultTagHandler;
-          var tags = handler(tagname, props);
-          parts[0] = tags[0];
-          parts.push(tags[1]); }
-      return parts.join('');
   }
   
 
